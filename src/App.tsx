298fb3d40140c5f5f5c8d87ca06afdee718f35ea
David <davidhurmanchuk@gmail.com>
2025-08-19 20:32:54 +0300

    fix for ai

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,35 @@ import React from 'react';
 
 type State = {
   pressedKey: string;
-}
+};
 export class App extends React.Component<{}, State> {
   state: State = {
     pressedKey: '',
-  }
+  };
 
   handleKeyUp = (event: KeyboardEvent) => {
-    this.setState({pressedKey: event.key})
-  }
+    this.setState({ pressedKey: event.key });
+  };
 
   componentDidMount() {
-    document.addEventListener('keyup', this.handleKeyUp)
+    document.addEventListener('keyup', this.handleKeyUp);
   }
 
-  componentDidUnmount() {
-    document.removeEventListener('keyup', this.handleKeyUp)
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
   }
 
   render() {
     const { pressedKey } = this.state;
 
-    return(<div className="App">
-      <p className="App__message">
-        {pressedKey ? `The last pressed key is [${pressedKey}]` : `Nothing was pressed yet`}
-      </p>
-    </div>)
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : `Nothing was pressed yet`}
+        </p>
+      </div>
+    );
   }
 }
